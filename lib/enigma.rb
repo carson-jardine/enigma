72e1841b4bbdfe9f3332f95808175aa7343491c4
Carson Jardine <carsonjardine@gmail.com>
2020-09-19 17:39:28 -0600

    Adds code to generate actual keys for each shift

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,5 @@
 class Enigma
-
+  #maybe move both key and date to module since they're both generating default values? Generatable?
   def default_key
     sprintf('%05d', rand(10 ** 5))
   end
@@ -8,7 +8,12 @@ class Enigma
     Date.today.strftime("%d%m%y")
   end
 
-  def encrypt(message, key, date)
+  def generate_key(key)
+    key.split('').each_cons(2).map do |pair|
+      pair.join.to_i
+    end
+  end
 
+  def encrypt(message, key = default_key, date = default_date)
   end
 end
