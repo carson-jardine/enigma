436e4a3b9bb913783f445cb2f71f0c23bdfaa68d
Carson Jardine <carsonjardine@gmail.com>
2020-09-19 21:58:40 -0600

    Complete encrypt reader/writer method and create encrypted txt file from CLI

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,7 +3,12 @@ require_relative 'enigma'
 message = File.open(ARGV[0], 'r')
 
 incoming_text = message.read
-
 message.close
+
 enigma = Enigma.new
 encrypted_text = enigma.encrypt(incoming_text)
+
+writer = File.open(ARGV[1], 'w')
+writer.write(encrypted_text[:encryption])
+
+puts "Created '#{ARGV[1]}' with the key #{encrypted_text[:key]} and date #{encrypted_text[:date]}"
