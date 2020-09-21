03725415524ab1f0361f962fcd6e986ddfb305bc
Carson Jardine <carsonjardine@gmail.com>
2020-09-21 13:00:56 -0600

    Create generatable module and add relevant code

---
+++ lib/generatable.rb
@@ -0,0 +1,44 @@
+module Generatable
+
+  def generate_keys(key)
+    key.split('').each_cons(2).map do |pair|
+      pair.join.to_i
+    end
+  end
+
+  def generate_offsets(date)
+    date_squared(date).digits.reverse[-4..-1]
+  end
+
+  def generate_shifts(keys, offsets)
+    keys.flat_map.with_index do |key, key_idx|
+      offsets.map.with_index do |offset, offset_idx|
+        key + offset if key_idx == offset_idx
+      end
+    end.compact
+  end
+
+  def shifts(key, date)
+    generate_shifts(generate_keys(key), generate_offsets(date))
+  end
+
+  def generate_encryption(message, key = default_key, date = default_date)
+    message.downcase.split('').map.with_index do |chr, index|
+      if character_set.include?(chr)
+        character_set.rotate(shifts(key, date)[index % 4])[character_set.index(chr)]
+      else
+        chr
+      end
+    end.join
+  end
+
+  def generate_decryption(ciphertext, key, date)
+    ciphertext.downcase.split('').map.with_index do |chr, index|
+      if character_set.include?(chr)
+        character_set.rotate(-shifts(key, date)[index % 4])[character_set.index(chr)]
+      else
+        chr
+      end
+    end.join
+  end
+end
